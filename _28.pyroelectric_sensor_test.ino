9d6134b1abb24d79e7650fda9c0e553754a74e53
LittleGiantBaek <horowitz@hanmail.net>
2016-11-24 04:11:49 +0900

    Add files via upload

---
+++ _28.pyroelectric_sensor_test.ino
@@ -0,0 +1,19 @@
+//pyroelectric sensor test
+
+int pyPin = 7;
+int ledPin = 13;
+
+void setup()
+{
+  pinMode(ledPin, OUTPUT);
+}
+
+void loop()
+{
+  if (digitalRead(pyPin) == 1) {
+    digitalWrite(ledPin, HIGH);
+  }
+  else {
+    digitalWrite(ledPin, LOW);
+  }
+}
