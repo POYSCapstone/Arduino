5829f32c6e4ea590bed2384cc9ddfac6bf10dcae
LittleGiantBaek <horowitz@hanmail.net>
2016-11-24 04:11:03 +0900

    Add files via upload

---
+++ 9. 광센서모듈-디지털/photoconductive_cell_sensor_test/photoconductive_cell_sensor_test.ino
@@ -0,0 +1,17 @@
+//photoconductive cell sensor test
+
+int photoresistancePin = 4;
+int ledPin = 13;
+
+void setup() {
+  pinMode(ledPin, OUTPUT);
+}
+
+void loop() {
+  if (digitalRead(photoresistancePin) == LOW) {
+    digitalWrite(ledPin, LOW);
+  }
+  else {
+    digitalWrite(ledPin, HIGH);
+  }
+}
