da113da4594ee693cdd19aec0a43f813a1513b5c
LittleGiantBaek <horowitz@hanmail.net>
2016-11-24 04:10:15 +0900

    .Sound_Sensor_test.ino

---
+++ _17.Sound_Sensor_test.ino
@@ -0,0 +1,18 @@
+//Sound Sensor test
+
+int soundPin = 2;
+int ledPin = 13;
+
+void setup()
+{
+  pinMode(ledPin, OUTPUT); 
+}
+
+void loop()
+{
+  if (digitalRead(soundPin) == LOW) {
+    digitalWrite(ledPin, HIGH);
+    delay(3000);
+  } else
+    digitalWrite(ledPin, LOW);
+}
